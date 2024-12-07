03b52d7030eebe2fb6e399be68d819f227a3e249
Kodo <47425225+KodoGaiden@users.noreply.github.com>
2024-12-07 15:57:09 -0500

    For loop to split the data into left and right side. For loop to get the total distance when comparing left and right.

--- Day1/main.py
+++ Day1/main.py
@@ -1,5 +1,18 @@
 #input = open("input.txt", "r")
 input = open("testinput.txt","r")
+leftColumn = []
+rightColumn = []
+totalDistance = 0
 
 for i in input:
-    print(i)
+    splitInput = i.split()
+    leftColumn.append(splitInput[0])
+    rightColumn.append(splitInput[1])
+leftColumn.sort(reverse=False)
+rightColumn.sort(reverse=False)
+
+for i in range(len(leftColumn)):
+    totalDistance += abs(int(leftColumn[i])-int(rightColumn[i]))
+    print(totalDistance)
+
+
