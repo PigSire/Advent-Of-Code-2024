f4771d552b15668c1f7372978d76f12eeed9446d
Kodo <47425225+KodoGaiden@users.noreply.github.com>
2024-12-07 16:04:57 -0500

    made the appent be int instead of str. added for loop for part two.

--- Day1/main.py
+++ Day1/main.py
@@ -1,18 +1,24 @@
-#input = open("input.txt", "r")
-input = open("testinput.txt","r")
+input = open("input.txt", "r")
+#input = open("testinput.txt","r")
 leftColumn = []
 rightColumn = []
 totalDistance = 0
+similarityScore = 0
 
 for i in input:
     splitInput = i.split()
-    leftColumn.append(splitInput[0])
-    rightColumn.append(splitInput[1])
+    leftColumn.append(int(splitInput[0]))
+    rightColumn.append(int(splitInput[1]))
 leftColumn.sort(reverse=False)
 rightColumn.sort(reverse=False)
 
 for i in range(len(leftColumn)):
-    totalDistance += abs(int(leftColumn[i])-int(rightColumn[i]))
-    print(totalDistance)
+    totalDistance += abs(leftColumn[i]-rightColumn[i])
+print(totalDistance)
+
+for i in range(len(leftColumn)):
+    timesFound = rightColumn.count(leftColumn[i])
+    similarityScore += leftColumn[i] * timesFound
+print(similarityScore)
 
 
