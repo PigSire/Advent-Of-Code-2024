bbd5826487b2b04dcd4ea65bbada850175438738
Kodo <47425225+KodoGaiden@users.noreply.github.com>
2024-12-07 16:20:26 -0500

    for loop find if we are going up or down

--- Day2/main.py
+++ Day2/main.py
@@ -1,5 +1,13 @@
 #input = open("input.txt", "r")
 input = open("testinput.txt", "r")
-
+direction = None
+line = []
 for i in input:
-    print(i)
+    line = i.split()
+    if line[0] > line[1]:
+        direction = "Decs"
+    elif line[0] < line[1]:
+        direction = "Incs"
+    else:
+        direction = "NoMove"
+    print(str(line) + direction)
