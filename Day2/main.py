e7212230f52f38414fe033a399e1471a81cea57b
Kodo <47425225+KodoGaiden@users.noreply.github.com>
2024-12-07 16:38:10 -0500

    for loop that checks to see if each floor is safe

--- Day2/main.py
+++ Day2/main.py
@@ -1,13 +1,33 @@
-#input = open("input.txt", "r")
-input = open("testinput.txt", "r")
+input = open("input.txt", "r")
+#input = open("testinput.txt", "r")
 direction = None
 line = []
+totalSafe = 0
 for i in input:
+    floorSafety = True
     line = i.split()
+    line = [int(num) for num in line]
     if line[0] > line[1]:
         direction = "Decs"
     elif line[0] < line[1]:
         direction = "Incs"
     else:
         direction = "NoMove"
-    print(str(line) + direction)
+
+    for i in range(len(line)-1):
+        if direction.__eq__("Incs"):
+            distance = line[i+1] - line[i]
+            if distance > 3 or distance < 1:
+                floorSafety = False
+                break
+        if direction.__eq__("Decs"):
+            distance = line[i] - line[i+1]
+            if distance > 3 or distance < 1:
+                floorSafety = False
+                break
+        if direction.__eq__("NoMove"):
+            floorSafety = False
+    if floorSafety:
+        totalSafe += 1
+    print(str(line) + direction +" "+ str(floorSafety))
+print(totalSafe)
